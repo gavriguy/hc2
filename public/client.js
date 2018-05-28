df7c10d27433dc6c1b871dc947ba7d59be28a60e
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2018-05-28 22:53:42 +0300

    initial commit

---
+++ public/client.js
@@ -0,0 +1,25 @@
+/* global TrelloPowerUp */
+
+console.log('aaa')
+
+var Promise = TrelloPowerUp.Promise;
+
+var onBtnClick = function (t, opts) {
+  t.modal({
+    url: './index.html',
+    fullscreen: false,
+    title: 'Hill Chart'
+  })
+};
+
+TrelloPowerUp.initialize({
+  'board-buttons': function (t, opts) {
+    return [
+      {
+      text: 'Hill Chart',
+      condition: 'always',
+      callback: onBtnClick,
+      }
+    ]
+  },
+});
