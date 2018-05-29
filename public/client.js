181ba685b1fc9456ad3aee1359cf2420d8b0cd31
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2018-05-29 14:58:35 +0300

    add card badges

--- public/client.js
+++ public/client.js
@@ -1,21 +1,41 @@
 /* global TrelloPowerUp */
 
-var onBtnClick = function (t, opts) {
+var onBtnClick = function(t, opts) {
   t.modal({
     url: './index.html',
     fullscreen: false,
     title: 'Hill Chart'
-  })
+  });
 };
 
 TrelloPowerUp.initialize({
-  'board-buttons': function (t, opts) {
+  'board-buttons': function(t, opts) {
     return [
       {
-      text: 'Hill Chart',
-      condition: 'always',
-      callback: onBtnClick,
+        text: 'Hill Chart',
+        condition: 'always',
+        callback: onBtnClick
       }
-    ]
+    ];
   },
+  'card-badges': function(t, opts) {
+    return t.get('board', 'shared').then(data => {
+      const { board, card } = t.getContext();
+      // console.log({ board, card, data });
+
+      // console.log('We just loaded the card name for fun: ' + id);
+      const isUnknown = data[card].x < 328;
+      return [
+        {
+          dynamic: function() {
+            return {
+              text: isUnknown ? 'Figuring things out' : 'Making it happen',
+              refresh: 10,
+              color: isUnknown ? 'orange' : 'green'
+            };
+          }
+        }
+      ];
+    });
+  }
 });
